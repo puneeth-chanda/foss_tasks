681bdfd757a2cb0215c875da353eefdae3de786c
puneeth2001 <puneethchanda2001@gmail.com>
2018-10-25 20:16:35 +0530

    Tweet

---
+++ tokens.py
@@ -0,0 +1,8 @@
+def consumer_key():
+	return("krklOEjpcPNJTFVf553xehNhi")
+def consumer_secret():
+	return("qZtbTKGDYBwJ8yI0AuRvOlVyQa1e11g2RvjdRmQsc2LzLN7wlX")
+def access_token():
+	return("1055369228130217984-AsL0Yu07HvGWStYKMqbY5LzXK1T6Fo")
+def access_token_secret():
+	return("y9Opz9fVQ0YgDxxCxpwSn2ryvSYV8w7QsmwBtkSkq0EIF") 
