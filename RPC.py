f1bc4199432ba05ad130cb4b58584e6d1662e953
puneeth2001 <puneethchanda2001@gmail.com>
2018-10-26 07:25:06 +0530

    ROCKPAPERSCISSORS

--- RPC.py
+++ RPC.py
@@ -7,27 +7,27 @@ a=["Rock","paper","Scissor"]
 w=int(input("How many turns is the Game:"))
 for i in range(0,w):
 	m=random.choice(a)
-	k=input()
+	k=input("Your Choice:")
 	if(m=="Rock" and k=="Scissor"):
 		cp=cp+1
-		print(m)
+		print("ComputerChoice:",m)
 	elif(m=="Rock" and k=="paper"):
 		sp=sp+1
-		print(m)
+		print("ComputerChoice:",m)
 	elif(m=="paper" and k=="Scissor"):
 		sp=sp+1
-		print(m)
+		print("ComputerChoice:",m)
 	elif(m=="paper" and k=="Rock"):
 		cp=cp+1
-		print(m)
+		print("ComputerChoice:",m)
 	elif(m=="Scissor" and k=="Rock"):
 		sp=sp+1
-		print(m)
+		print("ComputerChoice:",m)
 	elif(m=="Scissor" and k=="paper"):
 		cp=cp+1
-		print(m)
+		print("ComputerChoice:",m)
 	else:
-		print(m)
+		print("ComputerChoice:",m)
 print("Computer:",cp)
 print("player:",sp)
 if(cp>sp):
